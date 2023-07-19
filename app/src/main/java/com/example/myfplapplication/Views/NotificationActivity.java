24ca262a18371f7b5ce2156b5c0a1544c479fc7c
Lucas_Williams <101234675+LucasWilliams2622@users.noreply.github.com>
2023-07-19 20:16:23 +0700

    set up project

---
+++ app/src/main/java/com/example/myfplapplication/Views/NotificationActivity.java
@@ -0,0 +1,14 @@
+package com.example.myfplapplication.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.myfplapplication.R;
+import android.os.Bundle;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+}
---
+++ app/src/main/java/com/example/myfplapplication/Views/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.myfplapplication.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.myfplapplication.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
