1e5b4f40e8803ef75d3612961fe38f354fb62420
Lucas_Williams <101234675+LucasWilliams2622@users.noreply.github.com>
2023-07-20 09:03:29 +0700

    bottom nav

--- app/src/main/java/com/example/myfplapplication/MainActivity.java
+++ app/src/main/java/com/example/myfplapplication/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.myfplapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.myfplapplication.Views.HomeFragment;
+import com.example.myfplapplication.Views.NewsFragment;
+import com.example.myfplapplication.Views.ProfileFragment;
+import com.example.myfplapplication.Views.ScheduleFragment;
+import com.example.myfplapplication.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+                binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.schedule:
+                    replaceFragment(new ScheduleFragment());
+                    break;
+                case R.id.news:
+                    replaceFragment(new NewsFragment());
+                    break;
+            }
+            return true;
+        });
+
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
