01583b5b2d19222a2308defaf5f61acb1536ec02
tttthuy00 <thanhthuy280902@gmail.com>
2023-07-21 14:21:02 +0700

    Schedule

--- app/src/main/java/com/example/myfplapplication/Views/ScheduleFragment.java
+++ app/src/main/java/com/example/myfplapplication/Views/ScheduleFragment.java
@@ -1,66 +1,69 @@
 package com.example.myfplapplication.Views;
 
+import android.content.res.ObbInfo;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.myfplapplication.Adapter.FragmentScheduleAdapter;
 import com.example.myfplapplication.R;
+import com.example.myfplapplication.databinding.FragmentScheduleBinding;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ScheduleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ScheduleFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ScheduleFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ScheduleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ScheduleFragment newInstance(String param1, String param2) {
-        ScheduleFragment fragment = new ScheduleFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    FragmentScheduleBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        binding = FragmentScheduleBinding.inflate(inflater, container, false);
+        FragmentScheduleAdapter adapter = new FragmentScheduleAdapter(getChildFragmentManager(), getLifecycle());
+        binding.vpSchedule.setAdapter(adapter);
+
+        new TabLayoutMediator(binding.mTabSchedule, binding.vpSchedule, ((tab, position) -> {
+            switch (position){
+                case 0:
+                    tab.setText("Lịch Học");
+                    break;
+                case 1:
+                    tab.setText("Lịch Thi");
+                    break;
+            }
+        })).attach();
+
+        binding.mTabSchedule.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                binding.vpSchedule.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        binding.vpSchedule.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                binding.mTabSchedule.selectTab(binding.mTabSchedule.getTabAt(position));
+            }
+        });
+
+        return binding.getRoot();
     }
 }
