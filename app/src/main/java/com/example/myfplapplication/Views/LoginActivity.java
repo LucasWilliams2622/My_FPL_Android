4e40dcc0bad5ab2e1b76fc0f49483c3a1b80b7f4
Khanh Le <khanh.le@longdan.co.uk>
2023-07-21 11:59:10 +0700

    Add LoginActivity

---
+++ app/src/main/java/com/example/myfplapplication/Views/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.myfplapplication.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myfplapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
