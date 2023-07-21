6ae7e0d580376d84ee561437844452780f5c78b3
Khanh Le <khanh.le@longdan.co.uk>
2023-07-21 13:31:16 +0700

    Switch to MainActivity when click login

--- app/src/main/java/com/example/myfplapplication/Views/LoginActivity.java
+++ app/src/main/java/com/example/myfplapplication/Views/LoginActivity.java
@@ -2,15 +2,38 @@ package com.example.myfplapplication.Views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
+import com.example.myfplapplication.MainActivity;
 import com.example.myfplapplication.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private LinearLayout loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
