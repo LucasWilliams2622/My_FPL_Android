31076b2aaa3967b88d547b9bb7af0c015402a411
tttthuy00 <thanhthuy280902@gmail.com>
2023-07-22 20:04:28 +0700

    fix list ScheduleStudyFragment

---
+++ app/src/main/java/com/example/myfplapplication/Adapter/StudyAdapter.java
@@ -0,0 +1,124 @@
+package com.example.myfplapplication.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myfplapplication.Model.Study;
+import com.example.myfplapplication.Model.StudyGroup;
+import com.example.myfplapplication.R;
+
+import java.util.List;
+
+public class StudyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final int VIEW_TYPE_DATE = 0;
+    private static final int VIEW_TYPE_NOTIFICATION = 1;
+
+    private List<StudyGroup> studyGroups;
+
+    public StudyAdapter(List<StudyGroup> studyGroups) {
+        this.studyGroups = studyGroups;
+    }
+
+
+    @Override
+    public int getItemViewType(int position) {
+        int count = 0;
+        for (StudyGroup group: studyGroups) {
+            if (position == count) {
+                return VIEW_TYPE_DATE;
+            }
+            count++;
+            if (position < count + group.studies.size()) {
+                return VIEW_TYPE_NOTIFICATION;
+            }
+            count += group.studies.size();
+        }
+        throw new IllegalArgumentException("Invalid position");
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_DATE) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_subject_date, parent, false);
+            return new DateViewHolder(view);
+        } else if (viewType == VIEW_TYPE_NOTIFICATION) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_subject, parent, false);
+            return new StudyViewHolder(view);
+        } else {
+            throw new IllegalArgumentException("Invalid view type");
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        int count = 0;
+        for (StudyGroup group: studyGroups) {
+            if (position == count) {
+                ((DateViewHolder) holder).bind(group.date);
+                return;
+            }
+            count++;
+            if (position < count + group.studies.size()) {
+                ((StudyViewHolder) holder).bind(group.studies.get(position - count));
+                return;
+            }
+            count += group.studies.size();
+        }
+        throw new IllegalArgumentException("Invalid position");
+    }
+
+    @Override
+    public int getItemCount() {
+        int count = 0;
+        for (StudyGroup group: studyGroups) {
+            count += 1 + group.studies.size();
+        }
+        return count;
+    }
+
+    private static class DateViewHolder extends RecyclerView.ViewHolder {
+        private TextView dateView;
+
+        public DateViewHolder(@NonNull View itemView) {
+            super(itemView);
+            dateView = itemView.findViewById(R.id.date);
+        }
+
+        public void bind(String date) {
+            dateView.setText(date);
+        }
+    }
+
+    private static class StudyViewHolder extends RecyclerView.ViewHolder {
+        private TextView mon_hoc_id, ten_mon_hoc, dia_diem, ca, ngay_hoc;
+
+        public StudyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mon_hoc_id = itemView.findViewById(R.id.mon_hoc_id);
+            ten_mon_hoc = itemView.findViewById(R.id.ten_mon_hoc);
+            dia_diem = itemView.findViewById(R.id.dia_diem);
+            ca = itemView.findViewById(R.id.ca);
+            ngay_hoc = itemView.findViewById(R.id.ngay_hoc);
+        }
+
+        public void bind(Study study) {
+            mon_hoc_id.setText(study.mon_hoc_id);
+            ten_mon_hoc.setText(study.ten_mon_hoc);
+            dia_diem.setText(study.dia_diem);
+            ca.setText(study.ca);
+            ngay_hoc.setText(study.ngay_hoc);
+
+            // Load the image using your preferred image loading library
+        }
+    }
+}
--- app/src/main/java/com/example/myfplapplication/Views/Fragment/ScheduleStudyFragment.java
+++ app/src/main/java/com/example/myfplapplication/Views/Fragment/ScheduleStudyFragment.java
@@ -2,65 +2,69 @@ package com.example.myfplapplication.Views.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.myfplapplication.Adapter.NotificationAdapter;
+import com.example.myfplapplication.Adapter.StudyAdapter;
+import com.example.myfplapplication.Model.Notification;
+import com.example.myfplapplication.Model.NotificationGroup;
+import com.example.myfplapplication.Model.Study;
+import com.example.myfplapplication.Model.StudyGroup;
 import com.example.myfplapplication.R;
+import com.example.myfplapplication.databinding.FragmentScheduleStudyBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ScheduleStudyFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class ScheduleStudyFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ScheduleStudyFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ScheduleStudyFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ScheduleStudyFragment newInstance(String param1, String param2) {
-        ScheduleStudyFragment fragment = new ScheduleStudyFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    FragmentScheduleStudyBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule_study, container, false);
+        binding = FragmentScheduleStudyBinding.inflate(inflater, container, false);
+        // Create a list of notification groups
+        List<StudyGroup> studyGroups = new ArrayList<>();
+        studyGroups.add(new StudyGroup("2023-07-19", Arrays.asList(
+                new Study("MOB403", "Android Networking", "Ca 6", "T308 (Nhà T)", "19:30 - 21:30"),
+                new Study("MOB401", "Lập trình Mobile đa nền tảng", "Ca 2", "T1108 (Nhà T)", "9:30 - 11:30")
+        )));
+        studyGroups.add(new StudyGroup("2023-07-20", Arrays.asList(
+                new Study("MOB308", "Lập trình Game 2D", "Ca 2", "T1101 (Nhà T)", "9:30 - 11:30")
+        )));
+        studyGroups.add(new StudyGroup("2023-07-21", Arrays.asList(
+                new Study("MOB306", "Khởi sự doanh nghiệp", "Ca 6", "T306 (Nhà T)", "19:30 - 21:30"),
+                new Study("MOB402", "Lập trình Server cho Android", "Ca 6", "T1110 (Nhà T)", "19:30 - 21:30")
+        )));
+
+        // Create an adapter to populate the list with study groups
+        StudyAdapter studyAdapter = new StudyAdapter(studyGroups);
+
+        // Set the layout manager of the RecyclerView
+        binding.rvStudy.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // Set the adapter of the RecyclerView
+        binding.rvStudy.setAdapter(studyAdapter);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 }
